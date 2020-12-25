626cdd40bcc7de4c417e8dea88ce6c0a521e132d
khanhduy0112 <nguyenkhanhduy21123@gmail.com>
2020-12-25 19:26:43 +0700

    MAIL SERVER

---
+++ WebProject/src/main/java/com/nlu/utils/MailMessageUtil.java
@@ -0,0 +1,18 @@
+package com.nlu.utils;
+
+import com.mysql.cj.x.protobuf.MysqlxCrud;
+import com.nlu.model.Product;
+
+import java.util.List;
+
+public class MailMessageUtil {
+
+
+    public static String createOrderMessageConfirm(List<Object> objects) {
+        String result = "";
+
+
+        return result;
+    }
+
+}
